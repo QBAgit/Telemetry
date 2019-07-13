23bdb4b9afb216782500f7f54750d960e702f602
QBAgit <kubadrew@gmail.com>
2019-07-13 20:16:44 +0200

    FdataListView again using generics.ListCreateAPIView

--- TELE/Main/views.py
+++ TELE/Main/views.py
@@ -18,26 +18,14 @@ class MainView(View):
        return render(request, "all_sensors.html",ctx)
 
 
-class FdataListView(APIView):
+class FdataListView(generics.ListCreateAPIView):
     """
     List all float measures or create a new measure
     """
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
-    def get(self, request, format=None):
-        f_measures = Fdata.objects.all()
-        serializer = FdataSerializer(f_measures, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = FdataSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+    queryset = Fdata.objects.all()
+    serializer_class = FdataSerializer
 
 
 class FdataView(generics.RetrieveUpdateDestroyAPIView):
