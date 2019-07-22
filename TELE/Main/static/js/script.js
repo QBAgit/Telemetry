78c2c9dafe2f398cfcc4a46f0520c337ab514b52
QBAgit <kubadrew@gmail.com>
2019-07-22 15:20:43 +0200

    showBordData improved can display mult. collumns

--- TELE/Main/static/js/script.js
+++ TELE/Main/static/js/script.js
@@ -1,8 +1,7 @@
 function show_BordData(
     rest_api_url, 
     span_id,
-    collumn_names,
-    data_keys,
+    collumns,
     click_handler
     ){
 
@@ -24,53 +23,39 @@ function show_BordData(
     var fDivTitles = $('<div>'); //Row Div container
     fDivTitles.addClass('row');
 
-    var fDiv1 = $('<div>'); //Col Div container
-    fDiv1.addClass('col-sm-3');
-    fDiv1.text(collumn_names.COL_1);
-
-    var fDiv2 = $('<div>'); //Col Div container
-    fDiv2.addClass('col-sm-3');
-    fDiv2.text(collumn_names.COL_2);
-
-    var fDiv3 = $('<div>'); //Col Div container
-    fDiv3.addClass('col-sm-6');
-    fDiv3.text(collumn_names.COL_3);
-
-    // add to Main Div
-    fDivTitles.append(fDiv1,fDiv2,fDiv3);
+    for(var i=0;i<collumns.length;i++){
+        var colDiv = $('<div>');
+        colDiv.addClass(collumns[i].class);
+        colDiv.text(collumns[i].name);
+        fDivTitles.append(colDiv)
+    }
 
     // add to Container
     $main.append(fDivTitles);
 
     // Itterate with data
-    for(var i=0;i<data.length;i++){
+    for(var i=0;i<=data.length;i++){
         var item = data[i];
+        console.log(item);
 
-        // Create Bootstrap Grid
+            // Create Bootstrap Grid
         var fDivMain = $('<div>'); //Row Div container
         fDivMain.addClass('row');
         fDivMain.attr('sensor-id', item["id"]);
         fDivMain.click(click_handler);
         fDivMain.css('cursor','pointer');
 
-        var fDiv1 = $('<div>'); //Col Div container
-        fDiv1.addClass('col-sm-3');
-        fDiv1.text(item[data_keys.COL_1]);
-        
-        var fDiv2 = $('<div>'); //Col Div container
-        fDiv2.addClass('col-sm-3');
-        fDiv2.text(item[data_keys.COL_2]);
-
-        var fDiv3 = $('<div>'); //Col Div container
-        fDiv3.addClass('col-sm-6');
-        fDiv3.text(item[data_keys.COL_3]);
-
-        // add to Main Div
-        fDivMain.append(fDiv1,fDiv2,fDiv3);
+        for(var j=0;j<collumns.length;j++){
+            var colDiv = $('<div>');
+            colDiv.addClass(collumns[j].class);
+            colDiv.text(item[collumns[j].data_key]);
+            fDivMain.append(colDiv)
+        }
 
         // add to Container
         $main.append(fDivMain);
-    }   
+        }
+      
     });
 }
 
@@ -101,16 +86,15 @@ $(document).ready(function(){
         $Cont1 = $('#Cont1')
         $Cont1.removeClass("bg-1").addClass("bg-4")
 
-        // Wywolanie
         $(show_BordData(
             rest_api_url = '/api/v1/usersensors/',
             span_id = '#DashBordData',
-            collumn_names = {COL_1:"Name",COL_2:"Description",COL_3:"Value"},
-            data_keys = {COL_1:"name",COL_2:"description",COL_3:"value"},
+            collumns = [
+                {name:"Name",data_key:"name",class:"col-sm-3"},
+                {name:"Description",data_key:"description",class:"col-sm-6"},
+            ],
             click_handler = DashBordClick
             ));
-
-
     })
 
     var $Options_Link = $("#Options_Link");
@@ -124,12 +108,14 @@ $(document).ready(function(){
         $Cont1 = $('#Cont1')
         $Cont1.removeClass("bg-1").addClass("bg-4")
 
-        // Wywolanie
         $(show_BordData(
             rest_api_url = '/api/v1/usersensors/',
             span_id = '#OptionSens',
-            collumn_names = {COL_1:"ID",COL_2:"Name",COL_3:"Description"},
-            data_keys = {COL_1:"id",COL_2:"name",COL_3:"description"},          
+            collumns = [
+                {name:"ID",data_key:"id",class:"col-sm-3"},
+                {name:"Name",data_key:"name",class:"col-sm-3"},
+                {name:"Description",data_key:"description",class:"col-sm-6"}
+            ],
             click_handler = OptionClick
             ));
     })
