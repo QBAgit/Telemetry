637406afd1b0ff6a66566c08c7ab9bc40a0a5ae7
QBAgit <kubadrew@gmail.com>
2019-07-23 10:42:49 +0200

    cleanup

--- TELE/Main/views.py
+++ TELE/Main/views.py
@@ -41,33 +41,6 @@ class FdataView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = FdataSerializer
 
 
-# class FuserDataView(APIView):
-#     """
-#     List User float measures , or create new one.
-#     """
-#     def get(self, request, format=None):
-#         # Get all user sensors
-#         UserSensors = Sensor.objects.filter(owner = request.user)
-
-#         ListQSets = []
-
-#         # Get all sensors data
-#         for sen in UserSensors:
-#             ListQSets.append(Fdata.objects.filter(sensor = sen))
-        
-#         FUserData = list(chain(*ListQSets))
-
-#         serializer = FdataSerializer(FUserData, many=True)
-#         return Response(serializer.data)
-
-#     def post(self, request, format=None):
-#         serializer = FdataSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 class UserSensorList(generics.ListCreateAPIView):
     """
     List all float measures or create a new measure
--- TELE/TELE/urls.py
+++ TELE/TELE/urls.py
@@ -25,11 +25,8 @@ urlpatterns = [
     url(r'^$', views.MainView.as_view(), name="index"),
     url(r'^api/v1/fdata/$', views.FdataListView.as_view()), 
     url(r'^api/v1/fdata/(?P<pk>[0-9]+)/$', views.FdataView.as_view()),
-    # url(r'^api/v1/userfdata/$', views.FuserDataView.as_view()),
     url(r'^api/v1/usersensors/$', views.UserSensorList.as_view()),
     url(r'^api/v1/sensordata/(?P<pk>[0-9]+)/$', views.SensorfData.as_view()),
-    # url(r'^api/v1/users/$', views.UserList.as_view()),
-    # url(r'^api/v1/users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
 urlpatterns += [
