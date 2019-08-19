5da304d62b1d302a17ed336da684888a6f3ff76b
QBAgit <kubadrew@gmail.com>
2019-08-19 19:47:35 +0200

    add SensorManager to generate Token

--- TELE/Main/models.py
+++ TELE/Main/models.py
@@ -2,11 +2,58 @@ from django.db import models
 # from django.contrib.auth.models import User
 from registration.models import User
 # from django.utils.timezone import now
+from hashlib import pbkdf2_hmac
+from binascii import hexlify
+from hmac import compare_digest
+
+class SensorManager(models.Manager):
+    def create(self, **obj_data):
+        # Do some extra stuff here on the submitted data before saving...
+        # For example...
+        
+        obj_data['token'] = SensorManager.sign(obj_data['name'], obj_data['description'])
+
+        # Now call the super method which does the actual creation
+        return super().create(**obj_data) # Python 3 syntax!!
+    
+    @staticmethod
+    def sign(name,description):
+        AUTH_SIZE = 16
+        byte_sensor_name = bytes(str(name),'utf-8')
+        byte_description = bytes(str(description),'utf-8')
+        dk = pbkdf2_hmac('sha256',byte_sensor_name, byte_description, 100000, AUTH_SIZE)
+        return hexlify(dk)
+
 
 class Sensor(models.Model):
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=140)
     owner = models.ForeignKey(User, related_name='Sensor', on_delete=models.CASCADE)
+    token = models.CharField(max_length=32)
+
+    objects = SensorManager()
+
+    # @classmethod
+    # def sign(cls):
+
+    #     AUTH_SIZE = 16
+
+    #     byte_sensor_name = bytes(str(cls.name),'utf-8')
+    #     byte_description = bytes(str(cls.description),'utf-8')
+    #     dk = pbkdf2_hmac('sha256',byte_sensor_name, byte_description, 100000, AUTH_SIZE)
+    #     return hexlify(dk)
+
+    # @classmethod
+    # def verify(cls, sig):
+    #     good_sig = Sensor.sign(senor_name, sensor_description)
+    #     return compare_digest(good_sig, sig)
+
+    # @classmethod
+    # def create(cls, name, desc, owner):
+    #     sensor = cls(name = name, description = desc, owner = owner)
+    #     sensor.token = Sensor.sign()
+    #     return sensor
+
 
 class Fdata(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
