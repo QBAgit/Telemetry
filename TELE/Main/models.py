d540d5b94469920d6342822e792812253859ea50
QBAgit <kubadrew@gmail.com>
2019-08-21 15:14:32 +0200

    cleanup

--- TELE/Main/models.py
+++ TELE/Main/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-# from django.contrib.auth.models import User
 from registration.models import User
-# from django.utils.timezone import now
 from hashlib import pbkdf2_hmac
 from binascii import hexlify
 from hmac import compare_digest
@@ -27,7 +25,6 @@ class SensorManager(models.Manager):
         return super().create(**obj_data) # Python 3 syntax!!
 
 
-
 class Sensor(models.Model):
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=140)
@@ -35,27 +32,6 @@ class Sensor(models.Model):
     token = models.CharField(max_length=32)
 
     objects = SensorManager()
-
-    # @classmethod
-    # def generate(cls):
-
-    #     AUTH_SIZE = 16
-
-    #     byte_sensor_name = bytes(str(cls.name),'utf-8')
-    #     byte_description = bytes(str(cls.description),'utf-8')
-    #     dk = pbkdf2_hmac('sha256',byte_sensor_name, byte_description, 100000, AUTH_SIZE)
-    #     return hexlify(dk)
-
-    # @classmethod
-    # def verify(cls, sig):
-    #     good_sig = Sensor.generate(senor_name, sensor_description)
-    #     return compare_digest(good_sig, sig)
-
-    # @classmethod
-    # def create(cls, name, desc, owner):
-    #     sensor = cls(name = name, description = desc, owner = owner)
-    #     sensor.token = Sensor.generate()
-    #     return sensor
 
 
 class Fdata(models.Model):
