a12a5300b40d97a54d9b41f827dac0ba17a33ae5
QBAgit <kubadrew@gmail.com>
2019-08-22 12:40:06 +0200

    rest_api_post can gen sin/x^2

--- TELE/rest_api_post.py
+++ TELE/rest_api_post.py
@@ -3,7 +3,7 @@ import requests
 from random import randint
 import argparse
 import time
-
+import math
 
 BASE_API_URL = 'http://127.0.0.1:8000/api/v1/'
 
@@ -24,16 +24,37 @@ if __name__ == "__main__":
     parser.add_argument("-n", dest="n", required=True, help="number of values to be added")
     parser.add_argument("-id", dest="id", required=True, help="sensor id")
     parser.add_argument("-token", dest="token", required=True, help="sensor token")
-
+    parser.add_argument("-mode", dest="mode", required=False, help="sin, x^2"
+    )
     args = parser.parse_args()
 
-    if args.n and args.token and args.id: 
-        print("Add {} random values".format(args.n))
-        for item in range(int(args.n)):
-            random_number = randint(0,100)
-            measurement.update({"sensor":args.id, "token": args.token ,"value": random_number})
-            resp = add_fdata(measurement)
-            print(" * {} : {}".format(resp.status_code, resp.text))
-            time.sleep(5)
+    if args.n and args.token and args.id:
+        # Plot Math functoion
+        if args.mode:
+            print("Generate Math {} for {} samples".format(args.mode, args.n))
+            progress = 0
+            for item in range(int(args.n)):
+                
+                if args.mode == "sin":
+                    mathCurve = math.sin(item)
+                elif args.mode == "x^2":
+                    mathCurve = item*item
+
+                measurement.update({"sensor":args.id, "token": args.token ,"value": mathCurve})
+                resp = add_fdata(measurement)
+                progress += 1
+                print(" * -{}- :  {} : {}".format(progress, resp.status_code, resp.text))
+                # time.sleep(1)
+        # Plot random numbers
+        else:
+            print("Add {} random values".format(args.n))
+            progress = 0
+            for item in range(int(args.n)):
+                random_number = randint(0,100)
+                measurement.update({"sensor":args.id, "token": args.token ,"value": random_number})
+                resp = add_fdata(measurement)
+                progress += 1
+                print(" * -{}- :  {} : {}".format(progress, resp.status_code, resp.text))
+                # time.sleep(1)
     else:
         print("Missing parameters")
