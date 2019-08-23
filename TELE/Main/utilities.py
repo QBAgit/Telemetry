543ef06f7c9638f1e33f42b89dae9d44547ce7db
QBAgit <kubadrew@gmail.com>
2019-08-23 13:14:22 +0200

    adress code review findings

--- TELE/Main/utilities.py
+++ TELE/Main/utilities.py
@@ -8,5 +8,5 @@ class Token():
         AUTH_SIZE = 16
         byte_password = bytes(str(password),'utf-8')
         byte_salt = bytes(str(salt),'utf-8')
-        dk = pbkdf2_hmac('sha256',byte_password, byte_salt, 100000, AUTH_SIZE)
-        return hexlify(dk).decode('utf-8')
+        derivedKey = pbkdf2_hmac('sha256',byte_password, byte_salt, 100000, AUTH_SIZE)
+        return hexlify(derivedKey).decode('utf-8')
